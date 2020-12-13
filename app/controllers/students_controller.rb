605f66f30a071cae5dffb772b08c68b52a506208
Mark Ghaida <markghaida@gmail.com>
2020-12-13 00:37:49 -0500

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,17 +1,45 @@
 class StudentsController < ApplicationController
   
   def index
-    @students = Student.all
-  end
+    @students = Student.all 
+  end 
 
-  def show
+  def new 
+    @student = Student.new 
+  end 
+  
+  def create 
+   #byebug
+   @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+   redirect_to student_path(@student)
+  end 
+
+  def show 
     @student = Student.find(params[:id])
-  end
-
-  def new
-  end
-
-  def create
-  end
+  end 
 
 end
+
+#Errors I keep getting:
+
+# form page
+#   form renders with the new action
+#   new form submits content and renders form content (FAILED - 1)
+#   creates a record in the database (FAILED - 2)
+
+
+#   Failures:
+
+#   1) form page new form submits content and renders form content
+#      Failure/Error: fill_in 'first_name', with: "Margaery"
+     
+#      Capybara::ElementNotFound:
+#        Unable to find field "first_name"
+
+
+
+# 2) form page creates a record in the database
+#    Failure/Error: fill_in 'first_name', with: "Sansa"
+       
+#    Capybara::ElementNotFound:
+#      Unable to find field "first_name"
